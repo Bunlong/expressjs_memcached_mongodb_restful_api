a718aad546bd352e29e7099c69668e8de08aad8e
bunlong <bunlong.van@gmail.com>
2018-08-09 11:56:48 +0700

    build getting all questions api

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -1,11 +1,14 @@
 var Question = require('../models/question');
 
 exports.index = function(req, res) {
-  
+  Question.find(function (err, result) {
+    if (err) res.status(500).send({ data: null });
+    res.status(200).send({ data: result });
+  });
 };
 
 exports.show = function(req, res) {
-  Question.findById(req.params.id, function (err, result){
+  Question.findById(req.params.id, function (err, result) {
     if (err) res.status(500).send({ data: null });
     res.status(200).send({ data: result });
   });
