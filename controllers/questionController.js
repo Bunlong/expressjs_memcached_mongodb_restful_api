cb164f01c5f39dbd73a37e76429b3e2100542130
bunlong <bunlong.van@gmail.com>
2018-08-10 11:41:09 +0700

    removed styles space

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -1,35 +1,35 @@
 var Question = require('../models/question');
 
-exports.index = function(req, res) {
-  Question.find(function (err, result) {
+exports.index = function(req, res, next) {
+  Question.find(function(err, result) {
     if (err) res.status(500).send({ data: null });
     res.status(200).send({ data: result });
   });
 };
 
-exports.show = function(req, res) {
-  Question.findById(req.params.id, function (err, result) {
+exports.show = function(req, res, next) {
+  Question.findById(req.params.id, function(err, result) {
     if (err) res.status(500).send({ data: null });
     res.status(200).send({ data: result });
   });
 };
 
-exports.create = function(req, res) {
-  Question.create(req.body, function (err, result) {
+exports.create = function(req, res, next) {
+  Question.create(req.body, function(err, result) {
     if (err) res.status(500).send({ data: null });
     res.status(200).send({ data: result });
   });
 };
 
-exports.update = function(req, res) {
-  Question.findByIdAndUpdate(req.params.id, { $set: req.body}, { new: true }, function (err, result) {
+exports.update = function(req, res, next) {
+  Question.findByIdAndUpdate(req.params.id, { $set: req.body}, { new: true }, function(err, result) {
     if (err) res.status(500).send({ data: null });
     res.status(200).send({ data: result });
   });
 };
 
-exports.destroy = function(req, res) {
-  Question.findByIdAndRemove(req.params.id, function (err, result) {
+exports.destroy = function(req, res, next) {
+  Question.findByIdAndRemove(req.params.id, function(err, result) {
     if (err) res.status(500).send({ data: null });
     res.status(200).send({ data: result });
   });
